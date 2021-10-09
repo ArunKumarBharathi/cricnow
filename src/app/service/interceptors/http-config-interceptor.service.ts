5b96a6547c8cc59b702798eafbf0ab79c206e066
ArunKumarBharathi <88784162+ArunKumarBharathi@users.noreply.github.com>
2021-10-09 16:09:32 +0530

    Update http-config-interceptor.service.ts

--- src/app/service/interceptors/http-config-interceptor.service.ts
+++ src/app/service/interceptors/http-config-interceptor.service.ts
@@ -9,7 +9,7 @@ export class HttpConfigInterceptorService implements HttpInterceptor{
 
   constructor() { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    req = req.clone({headers:req.headers.append('x-rapidapi-key','13564b1849msh8fd880fe8ade726p1af044jsnb8e95dee4633')})
+    req = req.clone({headers:req.headers.append('x-rapidapi-key','0739770c89mshcc0b6d2595f61b4p1aa2e9jsnae1d119bc561')})
     req = req.clone({headers:req.headers.append('x-rapidapi-host','cricket-live-data.p.rapidapi.com')})
 
     return next.handle(req)
